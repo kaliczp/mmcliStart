c065587354a33bdd9014e1032495a3a2bffeb952
Péter Kalicz <kaliczp@gmail.com>
2026-06-13 21:04:41 +0200

    Get bearer number

--- mymodemStart.sh
+++ mymodemStart.sh
@@ -2,3 +2,5 @@ modem=`mmcli -L | awk -F "[" '{print $1}' | awk -F "/" '{print $6}'`
 mmcli -m $modem
 sudo mmcli -m $modem -e
 sudo mmcli -m $modem --simple-connect="apn=internet.telekom,ip-type=ipv4v6"
+bearer=`mmcli -m 0 | grep Bearer | awk -F "/" '{print $6}'`
+mmcli -b $bearer
