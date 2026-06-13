0f3a5b6815e5b93430bced13a86e6fcc7af12f31
Péter Kalicz <kaliczp@gmail.com>
2026-06-13 20:58:56 +0200

    Get modem number with mmcli and awk

--- mymodemStart.sh
+++ mymodemStart.sh
@@ -1,4 +1,4 @@
-modem=0
+modem=`mmcli -L | awk -F "[" '{print $1}' | awk -F "/" '{print $6}'`
 mmcli -m $modem
 sudo mmcli -m $modem -e
 sudo mmcli -m $modem --simple-connect="apn=internet.telekom,ip-type=ipv4v6"
