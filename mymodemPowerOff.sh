843ea55756e92aff7b405765f9399b4d29d1c229
Péter Kalicz <kaliczp@gmail.com>
2026-06-14 21:35:38 +0200

    Poweroff based now determined modem and bearer numbers

--- mymodemPowerOff.sh
+++ mymodemPowerOff.sh
@@ -1,7 +1,7 @@
 modem=`mmcli -L | awk -F "[" '{print $1}' | awk -F "/" '{print $6}'`
 sudo mmcli -m $modem --simple-disconnect
-bearer=`mmcli -m 0 | grep Bearer | awk -F "/" '{print $6}'`
-curripaddr=`mmcli -b 0 | grep address | awk -F ":" '{print $2}'`
+bearer=$(mmcli -m $modem | grep Bearer | awk -F "/" '{print $6}')
+curripaddr=$(mmcli -b $bearer | grep address | awk -F ":" '{print $2}')
 sudo mmcli -m $modem --delete-bearer=$bearer
 sudo mmcli -m $modem --disable
 sudo ip a del $curripaddr/32 dev wwan0
