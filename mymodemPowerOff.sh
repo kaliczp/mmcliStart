421a7c1565d9dab5e8632bf6201bfcbc84f99476
Péter Kalicz <kaliczp@gmail.com>
2026-06-14 15:58:17 +0200

    Correction of argument error
    Because different argument stiles, an error remainded, now corrected.

--- mymodemPowerOff.sh
+++ mymodemPowerOff.sh
@@ -3,7 +3,7 @@ sudo mmcli -m $modem --simple-disconnect
 bearer=`mmcli -m 0 | grep Bearer | awk -F "/" '{print $6}'`
 curripaddr=`mmcli -b 0 | grep address | awk -F ":" '{print $2}'`
 sudo mmcli -m $modem --delete-bearer=$bearer
-sudo mmcli -m=$modem --disable
+sudo mmcli -m $modem --disable
 sudo ip a del $curripaddr/32 dev wwan0
 sudo ip link set wwan0 down
 sudo mmcli -m $modem --set-power-state-off
