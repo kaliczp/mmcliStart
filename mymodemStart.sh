42ba2c26a0717e18d789957a7b0bc83d69b0bac4
Péter Kalicz <kaliczp@gmail.com>
2026-06-14 10:36:00 +0200

    Set IP after connection

--- mymodemStart.sh
+++ mymodemStart.sh
@@ -3,4 +3,8 @@ mmcli -m $modem
 sudo mmcli -m $modem -e
 sudo mmcli -m $modem --simple-connect="apn=internet.telekom,ip-type=ipv4v6"
 bearer=`mmcli -m 0 | grep Bearer | awk -F "/" '{print $6}'`
-mmcli -b $bearer
+# IP ASSIGN
+curripaddr=`mmcli -b 0 | grep address | awk -F ":" '{print $2}'`
+sudo ip link set wwan0 up
+sudo ip addr add $curripaddr/32 dev wwan0
+sudo ip route add default dev wwan0
