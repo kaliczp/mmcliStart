194e82060669356f16d285f0466cd1b72b399b29
Péter Kalicz <kaliczp@gmail.com>
2026-06-17 06:43:57 +0200

    Enhanced script with bearer test
    If clausure test the result of bearer is number if number disconnect, disable modem and remove IP adress

--- mymodemPowerOff.sh
+++ mymodemPowerOff.sh
@@ -1,9 +1,15 @@
 modem=`mmcli -L | awk -F "[" '{print $1}' | awk -F "/" '{print $6}'`
-sudo mmcli -m $modem --simple-disconnect
-bearer=$(mmcli -m $modem | grep Bearer | awk -F "/" '{print $6}')
-curripaddr=$(mmcli -b $bearer | grep address | awk -F ":" '{print $2}')
-sudo mmcli -m $modem --delete-bearer=$bearer
-sudo mmcli -m $modem --disable
-sudo ip a del $curripaddr/32 dev wwan0
-sudo ip link set wwan0 down
+# grep the Bearer line get the bearer number and remove white spaces with xargs
+bearer=$(mmcli -m $modem | grep Bearer | awk -F "/" '{print $6}' | xargs)
+# Reset the settings if bearer number found
+if [[ "$bearer" =~ ^[0-9]+$ ]]; then
+    sudo mmcli -m $modem --simple-disconnect
+    curripaddr=$(mmcli -b $bearer | grep address | awk -F ":" '{print $2}')
+    sudo mmcli -m $modem --delete-bearer=$bearer
+    sudo mmcli -m $modem --disable
+    sudo ip a del $curripaddr/32 dev wwan0
+    sudo ip link set wwan0 down
+else
+    echo "No bearer found. Modem poweroff"
+fi
 sudo mmcli -m $modem --set-power-state-off
